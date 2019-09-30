175890e2b85d29a9cd6828f857157e2f96eb35b6
hat3z <htzprdcts@gmail.com>
2019-09-30 18:12:10 +0200

    # 12.2 - IngredientRow prefab added
    - TODO: need complete Ingredient Buy button selectable amount functions

---
+++ Assets/Scripts/GamePlayUI Scripts/IngredientRowController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class IngredientRowController : MonoBehaviour
+{
+    public string ingredientID;
+    public TextMeshProUGUI IngNameLabel;
+    public TextMeshProUGUI IngStoredAmount;
+    public Image IngImage;
+
+    [Header("Buy settings")]
+    public GameObject BuyButtonPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
